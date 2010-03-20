d4c94e25dc5d43f9407c97d170fd56d422dd4fca
maryaRabarison <maryaRabarison@a0f1a0ca-2713-11df-a94c-d58e26249b9a>
2010-03-20 16:19:12 +0000

    création de qcm

---
+++ trunk/QCM Project/src/java/modele/Qcm.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package modele;
+
+/**
+ *
+ * @author maria Rabarison et Lou Ferrand
+ */
+public class Qcm {
+    private final Questionnaire questionnaire;
+    private int idUser;
+
+    public Qcm(Questionnaire questionnaire){
+        this.questionnaire=questionnaire;
+    }
+
+}
