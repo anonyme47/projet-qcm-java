6854943e11594eb7878c5792973bd9e990fa1647
ferrandlou <ferrandlou@a0f1a0ca-2713-11df-a94c-d58e26249b9a>
2010-03-20 21:37:09 +0000

    Ajout de QuestionnaireDAO et Database

--- QCM Project/src/java/util/QuestionnaireDAO.java
+++ QCM Project/src/java/util/QuestionnaireDAO.java
@@ -1,14 +1,72 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package util;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
- * @author Maria Rabarison et Lou Ferrand
+ * @author Lou
  */
 public class QuestionnaireDAO {
 
+    /**
+     * Récupère le questionnaire dont on connaît l'identifiant en base de données
+     * @param idQuestionnaire L'identifiant du questionnaire
+     * @return Le questionnaire identifié par idQuestionnaire
+     * @throws SQLException
+     */
+/*
+    public static Questionnaire getById(int idQuestionnaire) throws SQLException {
+        Questionnaire questionnaire = null;
+        Connection connexion = Database.getConnection();
+        String sql = "SELECT * FROM questionnaire WHERE id_questionnaire = ?";
+        PreparedStatement ordre = connexion.prepareStatement(sql);
+        ordre.setInt(1, idQuestionnaire);
+        ResultSet rs = ordre.executeQuery();
+
+        if (rs.next()) {
+            questionnaire = new Questionnaire(
+                    idQuestionnaire,
+                    rs.getInt("id_theme"),
+                    rs.getInt("id_niveau"),
+                    rs.getInt("id_user"),
+                    rs.getString("libelle"),
+                    rs.getDate("date_creation"),
+                    rs.getInt("limite_temps"),
+                    rs.getBoolean("est_actif"),
+                    QuestionnaireDAO.getQuestions(idQuestionnaire));
+        }
+        rs.close();
+        ordre.close();
+        connexion.close();
+        return questionnaire;
+    }
+*/
+    /**
+     * Récupérer la liste de toutes les questions relatives à un questionnaire
+     * @param idQuestionnaire L'identifiant du questionnaire dont on veut les questions
+     * @return La liste des questions incluses dans le questionnaire spécifié en paramètre
+     * @throws SQLException
+     */
+    public static ArrayList<Integer> getQuestions(int idQuestionnaire) throws SQLException {
+        ArrayList<Integer> questions = new ArrayList<Integer>();
+        Connection connexion = Database.getConnection();
+        String sql = "SELECT id_question FROM questionnaire_has_question";
+        sql += " WHERE id_questionnaire = ?";
+        PreparedStatement ordre = connexion.prepareStatement(sql);
+        ordre.setInt(1, idQuestionnaire);
+        ResultSet rs = ordre.executeQuery();
+
+        while (rs.next()) {
+            questions.add(rs.getInt("id_question"));
+        }
+        rs.close();
+        ordre.close();
+        connexion.close();
+        return questions;
+    }
 }
