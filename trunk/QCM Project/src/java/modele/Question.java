66d084ebe4f69a7def3edafca4498d5554031026
ferrandlou <ferrandlou@a0f1a0ca-2713-11df-a94c-d58e26249b9a>
2010-03-20 14:38:19 +0000

    Ajout des beans Question et Questionnaire

---
+++ trunk/QCM Project/src/java/modele/Question.java
@@ -0,0 +1,125 @@
+package modele;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Une question est créée par un utilisateur et peut faire partie d'un
+ * questionnaire (qui est une liste de question). Une question doit avoir au
+ * moins une réponse (qui sont contenues elles aussi dans une liste). Cette
+ * question est rattachée à un thème et à un libellé (qui serait par exemple:
+ * <em>"Quelle est la couleur du cheval blanc d'Henri IV ?"</em>).
+ * @author Maria Rabarison et Lou Ferrand
+ */
+public class Question {
+
+    private Integer idQuestion;
+    private String libelle;
+    private List<Reponse> reponses;
+    private int idTheme;
+    private int idUser;
+
+    /**
+     * Constructeur pour une nouvelle question
+     * @param idQuestion 
+     * @param libelle
+     * @param idTheme
+     * @param idUser
+     */
+    public Question(final Integer idQuestion, final String libelle, final int idTheme, final int idUser) {
+        assert idQuestion == null || idQuestion >= 0 : "idQuestion doit être non négatif (reçu: " + idQuestion + ")";
+        assert libelle != null && libelle.matches("^\\s*$") : "le libellé ne doit être ni null ni vide";
+        this.libelle = libelle;
+        this.idTheme = idTheme;
+        this.idUser = idUser;
+        this.reponses = new ArrayList<Reponse>();
+        assert invariant();
+    }
+
+    public Integer getIdQuestion() {
+        return idQuestion;
+    }
+
+    public void setIdQuestion(final int idQuestion) {
+        assert idQuestion > 0 : "Le numéro de la question doit être positif";
+        this.idQuestion = idQuestion;
+        assert invariant();
+    }
+
+    public int getIdTheme() {
+        return idTheme;
+    }
+
+    public void setIdTheme(final int idTheme) {
+        assert idTheme > 0 : "L'identifiant du thème doit être positif";
+        this.idTheme = idTheme;
+        assert invariant();
+    }
+
+    public int getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(final int idUser) {
+        assert idUser > 0 : "L'identifiant de l'utilisateur doit être positif";
+        this.idUser = idUser;
+        assert invariant();
+    }
+
+    public String getLibelle() {
+        return libelle;
+    }
+
+    public void setLibelle(final String libelle) {
+        assert libelle != null && !libelle.matches("^\\s*$") : "le libellé ne doit être ni null ni vide";
+        this.libelle = libelle;
+        assert invariant();
+    }
+
+    public List<Reponse> getReponses() {
+        return reponses;
+    }
+
+    public void setReponses(List<Reponse> reponses) {
+        assert reponses != null;
+        this.reponses = reponses;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Question other = (Question) obj;
+        if ((this.libelle == null) ? (other.libelle != null) : !this.libelle.equals(other.libelle)) {
+            return false;
+        }
+        if (this.idTheme != other.idTheme) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 37 * hash + (this.libelle != null ? this.libelle.hashCode() : 0);
+        hash = 37 * hash + this.idTheme;
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return "[ " + idQuestion + " : " + libelle + " : " + idTheme + " ]";
+    }
+
+    public boolean invariant() {
+        assert getIdQuestion() > 0 : "Le numéro de la question doit être positif";
+        assert getIdUser() > 0 : "L'identifiant de l'utilisateur doit être positif";
+        assert getLibelle() != null && !getLibelle().matches("^\\s*$") : "le libellé ne doit être ni null ni vide";
+        return true;
+    }
+}
--- trunk/QCM Project/src/java/modele/Questionnaire.java
+++ trunk/QCM Project/src/java/modele/Questionnaire.java
@@ -1,14 +1,182 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package modele;
 
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- *
- * @author Lou
+ * La classe Questionnaire est définie par un identifiant, un thème, un niveau,
+ * un utilisateur qui a créé le questionnaire, un libellé, la date de création,
+ * la limite de temps, le statut s'il est actif ou non et une collection de
+ * questions le constituant.
+ * 
+ * @author Maria Rabarison et Lou Ferrand
  */
 public class Questionnaire {
 
+    private Integer idQuestionnaire;
+    private String libelle;
+    private Date dateCreation;
+    private Integer limiteTemps;
+    private List<Integer> questions;
+    private boolean estActif;
+    private boolean estPasse;
+
+    private int idTheme;
+    private int idUser;
+    private int idNiveau;
+
+    /**
+     * Constructeur pour un nouveau questionnaire
+     * @param idQuestionnaire 
+     * @param libelle
+     * @param dateCreation
+     * @param idTheme
+     * @param idUser
+     * @param idNiveau
+     */
+    public Questionnaire(final Integer idQuestionnaire, final String libelle, 
+            final Date dateCreation, final int idTheme, final int idUser, final int idNiveau) {
+        this.libelle = libelle;
+        this.dateCreation = dateCreation;
+        this.idTheme = idTheme;
+        this.idUser = idUser;
+        this.idNiveau = idNiveau;
+        this.questions = new ArrayList<Integer>();
+        assert invariant();
+    }
+
+    public Date getDateCreation() {
+        return dateCreation;
+    }
+
+    public void setDateCreation(Date dateCreation) {
+        assert dateCreation != null;
+        this.dateCreation = dateCreation;
+    }
+
+    public boolean estActif() {
+        return estActif;
+    }
+
+    public void setEstActif(boolean estActif) {
+        this.estActif = estActif;
+    }
+
+    public boolean estPasse() {
+        return estPasse;
+    }
+
+    public void setEstPasse(boolean estPasse) {
+        this.estPasse = estPasse;
+    }
+
+    public int getIdNiveau() {
+        return idNiveau;
+    }
+
+    public void setIdNiveau(final int idNiveau) {
+        assert idNiveau > 0 : "L'identifiant du niveau doit être > 0";
+        this.idNiveau = idNiveau;
+        assert invariant();
+    }
+
+    public Integer getIdQuestionnaire() {
+        return idQuestionnaire;
+    }
+
+    public void setIdQuestionnaire(final Integer idQuestionnaire) {
+        assert idQuestionnaire == null || idQuestionnaire > 0 : "Le numéro du questionnaire doit être positif";
+        this.idQuestionnaire = idQuestionnaire;
+    }
+
+    public int getIdTheme() {
+        return idTheme;
+    }
+
+    public void setIdTheme(final int idTheme) {
+        assert idTheme > 0;
+        this.idTheme = idTheme;
+    }
+
+    public int getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(final int idUser) {
+        assert idUser > 0;
+        this.idUser = idUser;
+    }
+
+    public String getLibelle() {
+        return libelle;
+    }
+
+    public void setLibelle(final String libelle) {
+        assert libelle != null && !libelle.matches("^\\s*$");
+        this.libelle = libelle;
+    }
+
+    public Integer getLimiteTemps() {
+        return limiteTemps;
+    }
+
+    public void setLimiteTemps(final Integer limiteTemps) {
+        assert limiteTemps == null || limiteTemps > 0;
+        this.limiteTemps = limiteTemps;
+    }
+
+    public List<Integer> getQuestions() {
+        return questions;
+    }
+
+    public void setQuestions(final List<Integer> questions) {
+        assert questions != null;
+        this.questions = questions;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Questionnaire other = (Questionnaire) obj;
+        if ((this.libelle == null) ? (other.libelle != null) : !this.libelle.equals(other.libelle)) {
+            return false;
+        }
+        if (this.idTheme != other.idTheme) {
+            return false;
+        }
+        if (this.idNiveau != other.idNiveau) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 19 * hash + (this.libelle != null ? this.libelle.hashCode() : 0);
+        hash = 19 * hash + this.idTheme;
+        hash = 19 * hash + this.idNiveau;
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + libelle + ":" + idTheme + ":" + idNiveau + "]";
+    }
+
+    public boolean invariant() {
+        assert getLibelle() != null && !getLibelle().matches("^\\s*$");
+        assert getLimiteTemps() == null || getLimiteTemps() > 0;
+        assert getIdQuestionnaire() == null || getIdQuestionnaire() > 0 : "Le numéro du questionnaire doit être positif";
+        assert getIdUser() > 0;
+        assert getIdNiveau() > 0;
+        assert getIdTheme() > 0;
+        return true;
+    }
 }
