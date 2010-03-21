64a8a1fac78532ad1be0173fdacdcd01b0eaf566
ferrandlou <ferrandlou@a0f1a0ca-2713-11df-a94c-d58e26249b9a>
2010-03-21 13:12:17 +0000

    Ajout des tests, modification du base-QCM.xml, et modification de QuestionnaireDAO en consequence du changement dans la base de donnees

--- QCM Project/test/util/DatabaseTest.java
+++ QCM Project/test/util/DatabaseTest.java
@@ -1,7 +1,9 @@
 package util;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import org.junit.Test;
 import tools.QCMTestCase;
@@ -28,7 +30,7 @@ public class DatabaseTest extends QCMTestCase {
      * @throws Exception
      */
     @Test
-    public void testSelect() throws Exception {
+    public void testSelect() throws SQLException {
         System.out.println("testSelect");
         Connection connexion = Database.getConnection();
 //        String sql = "SELECT id_user, login, password FROM user WHERE id_user = 2 LIMIT 0,1";
@@ -49,5 +51,27 @@ public class DatabaseTest extends QCMTestCase {
         rs.close();
         ordre.close();
         connexion.close();
+    }
+
+    /**
+     * Test des contraintes sur Contenu
+     * Ce test est censé lever une exception car il est impossible d'avoir dans
+     * la table contenu la question 1 dans le questionnaire 1 une deuxieme fois
+     * @throws SQLException
+     */
+    @Test
+    public void testInsert() throws SQLException {
+        int idQuestionnaire = 1;
+        int idQuestion = 1;
+        Connection connexion = Database.getConnection();
+        String sql = "INSERT INTO contenu (id_questionnaire, id_question) VALUES (?, ?)";
+        PreparedStatement ordre = connexion.prepareStatement(sql);
+        ordre.setInt(1, idQuestionnaire);
+        ordre.setInt(2, idQuestion);
+        int ok = ordre.executeUpdate();
+        ordre.close();
+        connexion.close();
+        System.out.println(ok);
+        assertTrue(ok != 0);
     }
 }
