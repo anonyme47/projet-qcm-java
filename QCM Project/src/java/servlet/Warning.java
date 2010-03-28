ce56fd4eea57f14a9c25a4e4cd8e7b4690ea0ebf
maryaRabarison <maryaRabarison@a0f1a0ca-2713-11df-a94c-d58e26249b9a>
2010-03-28 19:20:45 +0000

    rajout de Warning

--- QCM Project/src/java/servlet/Warning.java
+++ QCM Project/src/java/servlet/Warning.java
@@ -6,21 +6,23 @@
 package servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.SQLException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import modele.User;
-import util.UserDAO;
+import modele.Questionnaire;
+import util.QuestionnaireDAO;
+import util.ThemeDAO;
 
 /**
  *
  * @author marya
  */
-public class Accueil extends HttpServlet {
-   
-    /** 
+public class Warning extends HttpServlet {
+
+    /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
      * @param request servlet request
      * @param response servlet response
@@ -29,11 +31,26 @@ public class Accueil extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
-        
-    } 
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        try {
+            /* TODO output your page here
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet Questionnaire</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet Questionnaire at " + request.getContextPath () + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+            */
+        } finally {
+            out.close();
+        }
+    }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
+    /**
      * Handles the HTTP <code>GET</code> method.
      * @param request servlet request
      * @param response servlet response
@@ -48,19 +65,27 @@ public class Accueil extends HttpServlet {
         response.setContentType("text/html; charset=UTF-8");
         try{
            if(request.getSession().getAttribute("user")!=null){
-                request.getRequestDispatcher("accueil.jsp").forward(request, response);
+                Questionnaire questionnaire = QuestionnaireDAO.getById(Integer.parseInt(request.getParameter("questionnaire").toString()));
+                request.setAttribute("questionnaire", questionnaire);
+                //request.setAttribute("theme",ThemeDAO.getById(questionnaire.getIdTheme()).getLibelle());
+                //request.setAttribute("theme",NiveauDAO.getById(questionnaire.getIdNiveau()).getLibelle());
+                request.setAttribute("theme","Theme");
+                request.setAttribute("niveau","niveau");
+                request.getRequestDispatcher("warning.jsp").forward(request, response);
             }else{
                 request.setAttribute("errorMessage", "Vous n'êtes pas connecté");
                 request.getRequestDispatcher("index.jsp").forward(request, response);
             }
+        }catch(SQLException e){
+            request.setAttribute("errorMessage", "Erreur interne :"+e.getMessage());
+            request.getRequestDispatcher("warning.jsp").forward(request, response);
         }catch(NullPointerException e){
-            request.setAttribute("errorMessage", "Vous n'êtes pas authentifié");
+            request.setAttribute("errorMessage", "Erreur :"+e.getMessage());
+            request.getRequestDispatcher("warning.jsp").forward(request, response);
         }
-        
-        
-    } 
+    }
 
-    /** 
+    /**
      * Handles the HTTP <code>POST</code> method.
      * @param request servlet request
      * @param response servlet response
@@ -70,58 +95,16 @@ public class Accueil extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
-        request.setCharacterEncoding("UTF-8");
-        response.setCharacterEncoding("UTF-8");
-        response.setContentType("text/html; charset=UTF-8");
-        User user= null;
-        String errorMessage = null;
-        String page = "index.jsp";
-        try {
-            int authentification = Integer.parseInt(request.getParameter("authentification").toString());
-            if(authentification != 1){
-                errorMessage = "Authentification non initialisée";
-            }else{
-                String login=request.getParameter("login").toString();
-                String password=request.getParameter("password").toString();
-                if(login==null || login.trim().isEmpty()){
-                    throw new IllegalArgumentException("Veuillez renseigner votre login");
-                }else if(password==null || password.trim().isEmpty()){
-                    throw new IllegalArgumentException("Veuillez renseigner votre mot de passe");
-                }
-                user= UserDAO.getByLoginAndPassword(login, password);
-                if(user==null){
-                    errorMessage = "Echec de l'authentification, login ou mot de passe incorrecte.";
-                }else{
-                    request.getSession().setAttribute("user",user);
-                    page="accueil.jsp";
-                }
-            }
-        }catch(SQLException e){
-            errorMessage = e.getMessage();
-        }catch(IllegalStateException e){
-            errorMessage=e.getMessage();
-        }catch(IllegalArgumentException e){
-            errorMessage=e.getMessage();
-        }catch(NullPointerException e){
-            errorMessage=e.getMessage();
-        } finally {
-            request.setAttribute("errorMessage", errorMessage);
-            request.getRequestDispatcher(page).forward(request, response);
-        }
+        processRequest(request, response);
     }
 
-    /** 
+    /**
      * Returns a short description of the servlet.
      * @return a String containing servlet description
      */
     @Override
     public String getServletInfo() {
-        return "Short description : "+this.getServletInfo();
+        return "Short description";
     }// </editor-fold>
 
 }
-/*
- * TODO
- * rajouter la page error_authentification
- * qu'est ce qu'il y a à mettre dans la variable de session concernant l'utilisateur?
- */
