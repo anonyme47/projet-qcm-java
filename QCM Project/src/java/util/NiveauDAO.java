7b89db31563e04b9d2fffef63c57766a3f8990a7
ferrandlou <ferrandlou@a0f1a0ca-2713-11df-a94c-d58e26249b9a>
2010-03-28 17:36:32 +0000

    Ajout des methodes getAll dans ThemeDAO et NiveauDAO (avec les tests unitaires qui passent)

--- QCM Project/src/java/util/NiveauDAO.java
+++ QCM Project/src/java/util/NiveauDAO.java
@@ -1,9 +1,9 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package util;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
 
 /**
  *
@@ -11,4 +11,16 @@ package util;
  */
 public class NiveauDAO {
 
+    public static HashMap<Integer, String> getAll() throws SQLException {
+        HashMap<Integer, String> niveaux = new HashMap<Integer, String>();
+        Connection connexion = Database.getConnection();
+        String sql = "SELECT id_niveau, libelle FROM niveau ORDER BY id_niveau ASC";
+        ResultSet rs = connexion.createStatement().executeQuery(sql);
+        while (rs.next()) {
+            niveaux.put(rs.getInt("id_niveau"), rs.getString("libelle"));
+        }
+        rs.close();
+        connexion.close();
+        return niveaux;
+    }
 }
--- QCM Project/src/java/util/ThemeDAO.java
+++ QCM Project/src/java/util/ThemeDAO.java
@@ -1,14 +1,27 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package util;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
 
 /**
  *
  * @author marya
  */
 public class ThemeDAO {
+
+    public static HashMap<Integer, String> getAll() throws SQLException {
+        HashMap<Integer, String> themes = new HashMap<Integer, String>();
+        Connection connexion = Database.getConnection();
+        String sql = "SELECT id_theme, libelle FROM theme ORDER BY id_theme ASC";
+        ResultSet rs = connexion.createStatement().executeQuery(sql);
+        while (rs.next()) {
+            themes.put(rs.getInt("id_theme"), rs.getString("libelle"));
+        }
+        rs.close();
+        connexion.close();
+        return themes;
+    }
 
 }
