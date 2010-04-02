2ecf71760ac88e1e9e7e6a639ac7013bda878ab1
maryaRabarison <maryaRabarison@a0f1a0ca-2713-11df-a94c-d58e26249b9a>
2010-04-02 19:18:59 +0000

    gestion des exceptions

--- trunk/QCM Project/src/java/exception/UnknownUserException.java
+++ trunk/QCM Project/src/java/exception/UnknownUserException.java
@@ -5,10 +5,19 @@
 
 package exception;
 
+
 /**
  *
  * @author marya
  */
-public class UnknownUserException {
+public class UnknownUserException extends Exception{
+    public static final Exception UnknownQuestionnaireException = new Exception("jhsjdf");
+    public static void main(String[] args){
+        try{
+            throw UnknownUserException.UnknownQuestionnaireException;
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
 
 }
