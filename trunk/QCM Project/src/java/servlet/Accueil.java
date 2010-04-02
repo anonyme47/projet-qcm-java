938b08d692924f5708d954159852a5b0dffac220
ferrandlou <ferrandlou@a0f1a0ca-2713-11df-a94c-d58e26249b9a>
2010-04-02 20:09:28 +0000

    Commit de Accueil et des Exceptions a gerer

--- trunk/QCM Project/src/java/servlet/Accueil.java
+++ trunk/QCM Project/src/java/servlet/Accueil.java
@@ -2,9 +2,9 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package servlet;
 
+import exception.QcmException;
 import java.io.IOException;
 import java.sql.SQLException;
 import javax.servlet.ServletException;
@@ -19,8 +19,8 @@ import util.UserDAO;
  * @author marya
  */
 public class Accueil extends HttpServlet {
-   
-    /** 
+
+    /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
      * @param request servlet request
      * @param response servlet response
@@ -28,11 +28,10 @@ public class Accueil extends HttpServlet {
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-        
-    } 
+            throws ServletException, IOException {
+    }
 
-    /** 
+    /**
      * Handles the HTTP <code>GET</code> method.
      * @param request servlet request
      * @param response servlet response
@@ -41,25 +40,28 @@ public class Accueil extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         request.setCharacterEncoding("UTF-8");
         response.setCharacterEncoding("UTF-8");
         response.setContentType("text/html; charset=UTF-8");
-        try{
-           if(request.getSession().getAttribute("user")!=null){
+        try {
+            String action = request.getParameter("action");
+            if (action.equals("logout")) {
+                request.getSession().invalidate();
+            } else if (request.getSession().getAttribute("user") != null) {
                 request.getRequestDispatcher("accueil.jsp").forward(request, response);
-            }else{
+            } else {
                 request.setAttribute("errorMessage", "Vous n'êtes pas connecté");
                 request.getRequestDispatcher("index.jsp").forward(request, response);
             }
-        }catch(NullPointerException e){
+        } catch (NullPointerException e) {
             request.setAttribute("errorMessage", "Vous n'êtes pas authentifié");
+        } catch (IllegalArgumentException e) {
+            request.setAttribute("errorMessage", "Erreur : " + e.getMessage());
         }
-        
-        
-    } 
+    }
 
-    /** 
+    /**
      * Handles the HTTP <code>POST</code> method.
      * @param request servlet request
      * @param response servlet response
@@ -68,59 +70,68 @@ public class Accueil extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
+        System.out.println("Trace: doPost§");
         request.setCharacterEncoding("UTF-8");
         response.setCharacterEncoding("UTF-8");
         response.setContentType("text/html; charset=UTF-8");
-        User user= null;
         String errorMessage = null;
-        String page = "index.jsp";
+        String page = "error.jsp";
         try {
-            int authentification = Integer.parseInt(request.getParameter("authentification").toString());
-            if(authentification != 1){
-                errorMessage = "Authentification non initialisée";
-            }else{
-                String login=request.getParameter("login").toString();
-                String password=request.getParameter("password").toString();
-                if(login==null || login.trim().isEmpty()){
-                    throw new IllegalArgumentException("Veuillez renseigner votre login");
-                }else if(password==null || password.trim().isEmpty()){
-                    throw new IllegalArgumentException("Veuillez renseigner votre mot de passe");
-                }
-                user= UserDAO.getByLoginAndPassword(login, password);
-                if(user==null){
-                    errorMessage = "Echec de l'authentification, login ou mot de passe incorrecte.";
-                }else{
-                    request.getSession().setAttribute("user",user);
-                    page="accueil.jsp";
-                }
+            String action = request.getParameter("action");
+            if (action.equals("authentification")) {
+                System.out.println("Avant le check user");
+                page = checkUser(request);
+                System.out.println("Après le check user");
             }
-        }catch(SQLException e){
+        } catch (SQLException e) {
             errorMessage = e.getMessage();
-        }catch(IllegalStateException e){
-            errorMessage=e.getMessage();
-        }catch(IllegalArgumentException e){
-            errorMessage=e.getMessage();
-        }catch(NullPointerException e){
-            errorMessage=e.getMessage();
+        } catch (IllegalStateException e) {
+            errorMessage = e.getMessage();
+        } catch (IllegalArgumentException e) {
+            errorMessage = e.getMessage();
+        } catch (NullPointerException e) {
+            errorMessage = e.getMessage();
         } finally {
             request.setAttribute("errorMessage", errorMessage);
             request.getRequestDispatcher(page).forward(request, response);
         }
     }
 
-    /** 
+    /**
      * Returns a short description of the servlet.
      * @return a String containing servlet description
      */
     @Override
     public String getServletInfo() {
-        return "Short description : "+this.getServletInfo();
+        return "Short description : " + this.getServletInfo();
     }// </editor-fold>
 
+    public String checkUser(HttpServletRequest request) throws SQLException {
+        String page = "error.jsp";
+        String errorMessage = null;
+        int authentification = Integer.parseInt(request.getParameter("authentification").toString());
+        if (authentification != 1) {
+            errorMessage = "Authentification non initialisée";
+        } else {
+            User user = null;
+            String login = request.getParameter("login").toString();
+            String password = request.getParameter("password").toString();
+            if (login == null || login.trim().isEmpty()) {
+                throw new IllegalArgumentException("Veuillez renseigner votre login");
+            } else if (password == null || password.trim().isEmpty()) {
+                throw new IllegalArgumentException("Veuillez renseigner votre mot de passe");
+            }
+            user = UserDAO.getByLoginAndPassword(login, password);
+            if (user == null) {
+                errorMessage = "Echec de l'authentification, login ou mot de passe incorrecte.";
+//                throw new QcmException().UnknownUserException;
+            } else {
+                request.getSession().setAttribute("user", user);
+                page = "accueil.jsp";
+            }
+        }
+        return page;
+    }
 }
-/*
- * TODO
- * rajouter la page error_authentification
- * qu'est ce qu'il y a à mettre dans la variable de session concernant l'utilisateur?
- */
+
