dca06e5f9f35f98752989b7e0680d542d2812c17
maryaRabarison <maryaRabarison@a0f1a0ca-2713-11df-a94c-d58e26249b9a>
2010-04-07 16:47:26 +0000

    QCMException

--- trunk/QCM Project/src/java/exception/QcmException.java
+++ trunk/QCM Project/src/java/exception/QcmException.java
@@ -1,22 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
 package exception;
 
 /**
  *
- * @author Maria Rabarison et Lou Ferrand
+ * @author marya
  */
-public class QcmException extends Exception {
-
-    /**
-     * Exception sur la gestion de l'utilisateur
-     */
-    public static final Exception UnknownUserException = new Exception("UnknownUserException : Echec de l'authentification, login ou mot de passe incorrect.");
-
-    public static final Exception UnknowQuestionnaireException = new Exception("UnknowQuestionnaireException : Le questionnaire n'existe pas.");
-
-    public static final Exception UnknowQuestionException = new Exception("UnknowQuestionException : La question n'existe pas.");
-
-    public static final Exception ExpiredSessionException = new Exception("ExpiredSessionException : Vous n'êtes pas connecté ou votre session a expiré.");
-
-    
+public class QcmException {
 
 }
